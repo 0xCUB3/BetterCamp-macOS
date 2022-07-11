128fc75419623232bd4b1d350bceccd800262a72
0xCUB3 <94565160+0xCUB3@users.noreply.github.com>
2022-07-11 08:38:19 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import osascript
+
+# 1) Welcome message
+# 2) Ask for type of installation (only supports USB for now)
+# 3) Ask for Windows ISO (to be added - Windows downloader tool)
+# 4) Ask for USB drive to install to
+# 5) Create USB Installer
+
+# Variables
+macModel = os.popen('sysctl hw.model').read().split(':')[1].strip()
+
+while True:
+    os.system('clear')
+    print('''
+##########################
+# Welcome to BetterCamp! #
+##########################
+
+This script will help you create a USB installer for Windows 7/8/8.1/10/11 and will prepare you with the necessary patches for your device.
+
+Your current model: ''' + macModel + '''
+If you wish to install to a different model, please select the "Change Model" option in the menu.
+
+1. Start Process
+2. Change Model
+3. Exit
+    ''')
+    x = input("Please select an option: ")
+    if x == '1':
+        break
+    elif x == '2':
+        break
+    elif x == '3':
+        exit()
+    else:
+        continue
