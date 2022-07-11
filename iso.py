9858d3b12035e4a14ffc777d258bd43929aef387
0xCUB3 <94565160+0xCUB3@users.noreply.github.com>
2022-07-11 12:14:35 -0400

    Begin ISO validation, add ISO picker

--- iso.py
+++ iso.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 
 def validate_iso():
-    iso_list = []
     while True:
+        iso_list = []
         os.system('clear')
         for file in Path(Path.home() / "Downloads").iterdir():
             try:
@@ -16,7 +16,8 @@ def validate_iso():
         for iso in iso_list:
             print(iso_list.index(iso) + 1, "-", iso)
         print("\n")
-        selection_num = input("Please select an ISO ({}-{}): ".format(iso_list.index(iso_list[0]) + 1, iso_list.index(iso_list[-1]) + 1))
-        if selection_num in (str(iso_list.index(iso_list[0]) + 1), str(iso_list.index(iso_list[-1]) + 1)):
-            globals.selected_iso = iso_list[int(selection_num) - 1]
+
+        selection_num = int(input("Please select an ISO ({}-{}): ".format(iso_list.index(iso_list[0]) + 1, iso_list.index(iso_list[-1]) + 1)))
+        if selection_num in range(iso_list.index(iso_list[0]) + 1, iso_list.index(iso_list[-1]) + 1):
+            globals.selected_iso = iso_list[selection_num - 1]
             break
