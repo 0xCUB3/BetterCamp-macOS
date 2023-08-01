5f0a39f692043788aee0bf26995e869cb15ffb96
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2023-08-01 09:52:48 -0400

    Added Boot Camp Links

--- src/global_vars.py
+++ src/global_vars.py
@@ -5,3 +5,8 @@ import os
 macModel = os.popen("sysctl hw.model").read().split(":")[1].strip()
 selected_iso = ""
 selected_usb = ""
+
+# Links to Bootcamp Drivers
+pre_2011 = "http://swcdn.apple.com/content/downloads/57/55/041-84868-A_402D0DFI39/6uz8hhgtd4b87t00mrq0uihiop5ivumsbk/BootCampESD.pkg"
+_2011_2012 = "http://swcdn.apple.com/content/downloads/57/55/041-84868-A_402D0DFI39/6uz8hhgtd4b87t00mrq0uihiop5ivumsbk/BootCampESD.pkg"
+post_2012 = "http://swcdn.apple.com/content/downloads/24/21/041-88430-A_MH044N1M88/l1tfz2ccakcy7ko79ugzcw0naadra581o3/BootCampESD.pkg"
